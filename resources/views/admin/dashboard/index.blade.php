549a9232f68b04a0c019205746b354d2854bd830
omega202500 <jsimo8388@gmail.com>
2026-04-07 18:09:57 +0100

    Modification du fichier index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -580,7 +580,7 @@
             border-bottom: 1px solid #3a3a42;
         }
 
-      
+
 
         /* Header avec logo amélioré */
        .sidebar-header {
@@ -1153,7 +1153,7 @@
    {{-- ===== TOPBAR MOBILE (visible uniquement sur mobile, EN DEHORS du container-fluid) ===== --}}
 <div class="mobile-topbar d-flex d-md-none align-items-center justify-content-between px-3 py-2">
     <div class="d-flex align-items-center gap-2">
-        <img src="{{ asset('images/icon.png') }}" alt="GestComm" class="logo-img-icon" style="width:38px;height:38px;">
+        <img src="{{ asset('images/icon.png') }}" alt="GestComm" class="logo-img-icon" style="width:40px;height:40px;">
         <span class="fw-bold text-white fs-5">GestComm</span>
     </div>
     <button class="btn btn-link text-white p-0 fs-2 lh-1" type="button"
