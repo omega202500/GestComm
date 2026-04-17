e0b710bbdc6cabe059696f7d668e05cfbbb40261
omega202500 <jsimo8388@gmail.com>
2026-04-17 14:03:41 +0100

    Fix: Force HTTPS and authentication

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
-class AppServiceProvider extends ServiceProvider
+public function boot()
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
     }
 }
