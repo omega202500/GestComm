bd99fe78f2acf9b863ec3900f2b85b3282114bfd
omega202500 <jsimo8388@gmail.com>
2026-04-17 14:25:39 +0100

    Fix: Force HTTPS URL in config/app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://gestcomm-w05j.onrender.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
