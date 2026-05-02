9a5ec5eb5a4a6cd487ced18cd3a6b68873c8ddd7
omega202500 <jsimo8388@gmail.com>
2026-05-02 08:00:35 +0100

    Refactor count method to return total client count

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -78,11 +78,9 @@ class ClientController extends Controller
         $statistiques = $this->clientService->getStatistiquesClientsParZone();
         return view('clients.statistiques', compact('statistiques'));
     }
-    public function count() {
-    return response()->json([
-        'success' => true,
-        'count'   => Client::whereDate('created_at', today())->count()
-    ]);
+ public function count()
+{
+    return response()->json(['count' => Client::count()]);
 }
 
 }
